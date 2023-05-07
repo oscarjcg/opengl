a8c449cbc91e53de92704b5b43ed4aa8c9c05d7a
Oscar Catari Gutierrez <oscarcatari@outlook.es>
2023-05-07 17:05:17 +0100

    Add ambient lighting

--- OpenGLCourseApp/Shader.cpp
+++ OpenGLCourseApp/Shader.cpp
@@ -80,6 +80,8 @@ void Shader::CompileShader(const char* vertexCode, const char* fragmentCode)
 	uniformProjection = glGetUniformLocation(shaderID, "projection");
 	uniformModel = glGetUniformLocation(shaderID, "model");
 	uniformView = glGetUniformLocation(shaderID, "view");
+	uniformAmbientColour = glGetUniformLocation(shaderID, "directionalLight.colour");
+	uniformAmbientIntensity = glGetUniformLocation(shaderID, "directionalLight.ambientIntensity");
 }
 
 GLuint Shader::GetProjectionLocation()
@@ -93,6 +95,14 @@ GLuint Shader::GetModelLocation()
 GLuint Shader::GetViewLocation()
 {
 	return uniformView;
+}
+GLuint Shader::GetAmbientColourLocation()
+{
+	return uniformAmbientColour;
+}
+GLuint Shader::GetAmbientIntensityLocation()
+{
+	return uniformAmbientIntensity;
 }
 
 void Shader::UseShader()
